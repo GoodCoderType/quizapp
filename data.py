4f09e902ac82ae04acc1353a70888aa1ba21699e
GoodCoderType <goodgamerontop@gmail.com>
2024-08-09 13:19:44 +0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,502 @@
+question_data =  [
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "Several characters in &quot;Super Mario 64&quot; blink their eyes, including Mario himself.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Music",
+"question": "The song &quot;Mystery Train&quot; was released by artist &quot;Little Junior&#039;s Blue Flames&quot; in 1953.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Film",
+"question": "Shaquille O&#039;Neal appeared in the 1997 film &quot;Space Jam&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Geography",
+"question": "Liechtenstein does not have an airport.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Music",
+"question": "Green Day&#039;s album &#039;American Idiot&#039; is considered a &quot;punk rock opera.&quot;",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "History",
+"question": "Sir Issac Newton served as a Member of Parliament, but the only recorded time he spoke was to complain about a draft in the chambers.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "The game &quot;Jetpack Joyride&quot; was created by &quot;Redbrick Studios&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Science &amp; Nature",
+"question": "The &quot;Gympie Stinger&quot; is the deadliest plant in the world.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Computers",
+"question": "The logo for Snapchat is a Bell.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Video Games",
+"question": "In the Resident Evil series, Leon S. Kennedy is a member of STARS.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Vehicles",
+"question": "The term &quot;GTO&quot; was originated by Ferrari?",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "General Knowledge",
+"question": "Albert Einstein had trouble with mathematics when he was in school.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Video Games",
+"question": "Resident Evil 4 was originally meant to be a Nintendo GameCube exclusive.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Video Games",
+"question": "The video game &quot;Fuel&quot; has an open world that is 5,560 square miles?",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Japanese Anime &amp; Manga",
+"question": "In &quot;To Love-Ru: Darkness&quot;, Yami reveals her real name is Eve.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "History",
+"question": "Brezhnev was the 5th leader of the USSR.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Vehicles",
+"question": "The Japanese Shinkansen beat the French TGV&#039;s speed record for fastest electric rail train.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Television",
+"question": "In &quot;Star Trek&quot;, Klaang is a typical Klingon male.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Geography",
+"question": "The Sonoran Desert is located in eastern Africa.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Video Games",
+"question": "DragonForce&#039;s &#039;Through the Fire and Flames&#039; is widely considered to be the hardest song in the Guitar Hero series.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Politics",
+"question": "Jeb Bush was elected as Governor of Florida in 2002, starting his political career.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Video streaming website YouTube was purchased in it&#039;s entirety by Facebook for US$1.65 billion in stock.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Art",
+"question": "Venus of Willendorf is one of the earliest works of art, depicting the planets Mars and Venus embrace in the heavens at night.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "History",
+"question": "In 1967, a magazine published a story about extracting hallucinogenic chemicals from bananas to raise moral questions about banning drugs.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The National Animal of Scotland is the Unicorn.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Film",
+"question": "In the original Star Wars trilogy, Alec Guinness provided the voice for Darth Vader.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Video Games",
+"question": "Hidden in the files for &quot;Mario Kart Arcade GP&quot; is a picture of the Beslan school hostage crisis.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Television",
+"question": "The television show Doctor Who first aired in 1963.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Cartoon &amp; Animations",
+"question": "Nickelodeon rejected the pilot to Adventure Time.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Vehicles",
+"question": "The majority of Subaru vehicles are made in China.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Animals",
+"question": "In 2016, the IUCN reclassified the status of Giant Pandas from endangered to vulnerable.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Science: Gadgets",
+"question": "The communication protocol NFC stands for Near-Field Control.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "General Knowledge",
+"question": "&quot;Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.&quot; is a grammatically correct sentence.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "In &quot;Super Mario Bros.&quot;, the clouds and bushes have the same artwork and are just different colors.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "When you cry in space, your tears stick to your face.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Japanese Anime &amp; Manga",
+"question": "The protagonist in &quot;Humanity Has Declined&quot; has no discernable name and is simply referred to as &#039;I&#039; for most of the series.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Geography",
+"question": "Seoul is the capital of North Korea.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Geography",
+"question": "Greenland is almost as big as Africa.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "Minecraft can be played with a virtual reality headset.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "&quot;Resident Evil 7&quot; is the first first-person Resident Evil game.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Film",
+"question": "The original ending of &quot;Little Shop Of Horrors&quot; has the plants taking over the world.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Books",
+"question": "Shub-Niggurath is a creature that was created by \tJ. R. R. Tolkien in his novel &quot;The Lord of The Rings&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Television",
+"question": "Klingons respect their disabled comrades, and those who are old, injuried, and helpless.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Film",
+"question": "The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom Anderson.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Film",
+"question": "Ewan McGregor did not know the name of the second prequel film of Star Wars during and after filming.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "The ghosts in &quot;Pac-Man&quot; and &quot;Ms. Pac-Man&quot; have completely different behavior.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Ping-Pong originated in England",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Music",
+"question": "Michael Jackson wrote The Simpsons song &quot;Do the Bartman&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Japanese Anime &amp; Manga",
+"question": "In the &quot;Kagerou Daze&quot; series, Shintaro Kisaragi is prominently shown with the color red.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "The mitochondria is the powerhouse of the cell.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
---
+++ ui.py
@@ -0,0 +1,54 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#454545"
+
+class App:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quiz Bot")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.score_label = Label(text="Score : 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+        self.canvas = Canvas(width=300, height=250, bg="blue")
+        self.question_text = self.canvas.create_text(
+            150, 125, width=280, text="Kya Ghost Ky Tatty\n2 hain?", fill=THEME_COLOR, font=("Arial", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2)
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=8, command=self.true_button_work)
+        self.true_button.grid(row=2, column=0)
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=8, command=self.false_button_work)
+        self.false_button.grid(row=2, column=1)
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="blue")
+        self.score_label.config(text=f"Score : {self.quiz.score}/{self.quiz.question_number}")
+        if self.quiz.still_has_questions():
+            ttext = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=ttext)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've completed the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_button_work(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_button_work(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self,answer):
+        if answer == True:
+            self.canvas.itemconfig(self.question_text,text="Valid Answer")
+            self.canvas.config(bg="green")
+        elif answer == False:
+            self.canvas.itemconfig(self.question_text,text="Invalid Answer")
+            self.canvas.config(bg="red")
+        self.window.after(500, self.get_next_question)
